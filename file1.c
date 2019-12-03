ac0d4c31f6bca5067a688362cdb683a6c5f3bd20
sonali-mandloi <engsonalimandloi@gmail.com>
2019-12-03 02:00:29 -0800

    file1 and file 2 commited

---
+++ file1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a=10;
+int b=20;
+int sum=a+b;
+printf("sum is:%d",sum);
+
+}
---
+++ file2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a=10;
+int b=20;
+int sum=a-b;
+printf("sub is:%d",sum);
+
+}
+
